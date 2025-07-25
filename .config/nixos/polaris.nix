dd9e2e1eb3fca0054bd50ddc8f7334ea0df30f08
Dhairya Shah <dhaishah7@gmail.com>
2025-07-25 14:03:05 -0400

    Setup minecraft

--- .config/nixos/polaris.nix
+++ .config/nixos/polaris.nix
@@ -4,7 +4,9 @@
   pkgs,
   modulesPath,
   ...
-}: {
+}: let
+  nix-minecraft = builtins.getFlake "github:Infinidoge/nix-minecraft";
+in {
   services.printing.enable = true;
 
   services.pipewire = {
@@ -104,4 +106,43 @@
   services.udisks2.enable = true;
 
   services.flatpak.enable = true;
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  xdg.mime.defaultApplications = {
+    "text/html" = "app.zen_browser.zen.desktop";
+    "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
+    "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+    "x-scheme-handler/about" = "app.zen_browser.zen.desktop";
+    "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
+  };
+
+  imports = [nix-minecraft.nixosModules.minecraft-servers];
+  nixpkgs.overlays = [nix-minecraft.overlay];
+
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+
+    servers.gale = {
+      enable = true;
+      package = pkgs.paperServers.paper-1_21_8;
+
+      serverProperties = {
+        gamemode = "survival";
+        difficulty = "easy";
+        simulation-distance = 4;
+        level-seed = "67_34shjbfabhj37";
+      };
+
+      jvmOpts = [
+        "-Xms1G" # Minimum RAM (adjust based on your server's resources)
+        "-Xmx2G" # Maximum RAM (adjust based on your server's resources)
+        "-XX:+UseG1GC"
+      ];
+    };
+  };
+
+  networking.firewall.allowedUDPPorts = [19132]; # Default GeyserMC Bedrock port
+  networking.firewall.allowedTCPPorts = [25565];
 }
