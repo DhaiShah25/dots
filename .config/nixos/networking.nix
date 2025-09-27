508d09e09b7e2c4aef64cd6a3dec2ad0f32c9d0b
Dhairya Shah <dhaishah7@gmail.com>
2025-09-27 19:09:41 -0400

    Switched to dnscrypt-proxy2 bc dnsovertls had issues with systemd-resolved

--- .config/nixos/networking.nix
+++ .config/nixos/networking.nix
@@ -13,13 +13,6 @@
   networking.firewall.allowedTCPPorts = [22000 8080 3000 5000 23231 8888];
   networking.firewall.allowedUDPPorts = [22000];
 
-  services.resolved = {
-    enable = true;
-    dnssec = "true";
-    domains = ["~."];
-    fallbackDns = ["9.9.9.9" "149.112.112.112"];
-  };
-
   networking.networkmanager.enable = true;
   networking.wireless.iwd.enable = true;
 
@@ -32,8 +25,24 @@
     };
   };
 
-  networking.nameservers = ["9.9.9.9" "149.112.112.112"];
+  networking.nameservers = ["127.0.0.1"];
 
   networking.networkmanager.wifi.backend = "iwd";
-  networking.networkmanager.dns = "systemd-resolved";
+  networking.networkmanager.dns = "none";
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = false;
+      require_dnssec = true;
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+    };
+  };
 }
