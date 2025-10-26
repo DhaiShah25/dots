8db0ff29c9779f6176506332dd6810c37b460abf
Dhairya Shah <dhaishah7@gmail.com>
2025-10-26 11:03:22 -0400

    Kernel 6.17.3 & 6.17.4 hangs systemd-udevd

--- .config/nixos/polaris.nix
+++ .config/nixos/polaris.nix
@@ -8,7 +8,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
