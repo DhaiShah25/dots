cd6c9d65b9e2bc3bfff40cf647679b72da25e6e6
Dhairya Shah <dhaishah7+git@gmail.com>
2026-08-17 10:47:41 -0400

    Switch to evergarden

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,8 +14,3 @@ vim.filetype.add({
 		djot = "djot",
 	},
 })
-
--- Transparent colors w/ catppuccin
-vim.cmd.colorscheme("catppuccin")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
---
+++ .config/nvim/lua/plugins/evergarden.lua
@@ -0,0 +1,21 @@
+return {
+	"gh:everviolet/nvim.git",
+	name = "evergarden",
+	setup = function()
+		vim.opt.termguicolors = true
+		require("evergarden").setup({
+			theme = {
+				variant = "fall",
+				accent = "blue",
+			},
+			editor = {
+				transparent_background = true,
+			},
+		})
+		if vim.g.neovide then
+			vim.g.neovide_scroll_animation_length = 0.1
+			vim.g.neovide_opacity = 0.8
+		end
+		vim.cmd.colorscheme("evergarden")
+	end,
+}
